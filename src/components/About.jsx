6ce13cce14f45150ad230a22ed6120941074981b
ThevinduD <thevindud.work@gmail.com>
2025-09-18 20:15:21 +0530

    Initial Commit

---
+++ src/components/About.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div id='about' className='w-screen min-h-screen'>
+        <div className='relative mb-8 mt-36 flex flex-col items-center gap-5'>
+            <h2 className='font-general text-sm uppercase md:text-[10px]'>Welcome to Zentry</h2>
+            <div className='mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]'>
+                Disc<b>o</b>ver the world's l<b>a</b>rgest shared adventure
+            </div>
+
+            <div className='about-subtext'> 
+                <p>The Game of Games Begins - your life, now an epic MMORPG</p>
+                <p>Zentry unites every player from countless games and platforms</p>
+            </div>
+        </div>
+
+        <div id='clip' className='h-dvh w-screen'> 
+            <div className='mask-clip-path about-image'>
+                <img
+                    src='img/about.webp'
+                />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
