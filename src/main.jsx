95036604551115fd4f7585d6963bede31f88d5c6
ThevinduD <thevindud.work@gmail.com>
2025-09-22 13:17:57 +0530

    fix: Optimizations

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'lenis/dist/lenis.css'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <SpeedInsights/>
   </StrictMode>,
 )
